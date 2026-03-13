0ffabd15af4d22fde9c9d99628ae96dad5872a59
Nicolò Panunzio <164801182+npanu420@users.noreply.github.com>
2026-03-13 20:14:45 +0100

    version name + icon .ico

---
+++ scripts/sync-version.js
@@ -0,0 +1,22 @@
+/**
+ * Copia la versione da config/appVersion.json a package.json.
+ * Unico punto da modificare per la versione: config/appVersion.json
+ */
+const fs = require('fs');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+const configPath = path.join(root, 'config', 'appVersion.json');
+const pkgPath = path.join(root, 'package.json');
+
+const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+const version = config.version;
+if (!version || typeof version !== 'string') {
+  console.error('[sync-version] config/appVersion.json must have a "version" string');
+  process.exit(1);
+}
+
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+pkg.version = version;
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n', 'utf8');
+console.log('[sync-version] package.json version set to', version);
